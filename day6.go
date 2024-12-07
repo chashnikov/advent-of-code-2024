740cfcf95c80af3816be4ca8b5c61394a1616896
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-07 23:02:07 +0100

    day 6: solve part 2

--- day6.go
+++ day6.go
@@ -6,28 +6,54 @@ import (
 )
 
 func day6(fileName string) {
-	grid := Map(ReadLines(fileName), func(s string) []byte {
+	grid0 := Map(ReadLines(fileName), func(s string) []byte {
 		result := make([]byte, len(s))
 		for i, c := range s {
 			result[i] = byte(c)
 		}
 		return result
 	})
-	width := len(grid[0])
-	height := len(grid)
+	width := len(grid0[0])
+	height := len(grid0)
 
 	isInitial := func(c byte) bool { return c == '^' }
-	y := slices.IndexFunc(grid, func(s []byte) bool {
+	y0 := slices.IndexFunc(grid0, func(s []byte) bool {
 		return Any(s, isInitial)
 	})
-	x := slices.IndexFunc(grid[y], isInitial)
+	x0 := slices.IndexFunc(grid0[y0], isInitial)
+	grid := Map(grid0, slices.Clone)
+	simulateGuard(grid, x0, y0, width, height)
+
+	result := 0
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			if grid[y][x] != 'X' || x == x0 && y == y0 {
+				continue
+			}
+			newGrid := Map(grid0, slices.Clone)
+			newGrid[y][x] = '#'
+			if simulateGuard(newGrid, x0, y0, width, height) {
+				result++
+			}
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func simulateGuard(grid [][]byte, x int, y int, width int, height int) bool {
 	dirIndex := 0
+	moves := 0
 	for {
+		moves++
+		if moves > width*height*4 {
+			return true
+		}
 		grid[y][x] = 'X'
 		nx := x + directions[dirIndex].dx
 		ny := y + directions[dirIndex].dy
 		if nx < 0 || nx >= width || ny < 0 || ny >= height {
-			break
+			return false
 		}
 		if grid[ny][nx] == '#' {
 			dirIndex = (dirIndex + 1) % 4
@@ -36,12 +62,6 @@ func day6(fileName string) {
 		x = nx
 		y = ny
 	}
-	result := Sum(Map(grid, func(row []byte) int {
-		return Count(row, func(b byte) bool {
-			return b == 'X'
-		})
-	}))
-	fmt.Println(result)
 }
 
 var directions = []direction{
