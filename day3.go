982b5a71afbebbc0a891d87e4a88d65fb53e2d80
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-07 20:19:01 +0100

    day 3: solve part 2

--- day3.go
+++ day3.go
@@ -3,15 +3,31 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func day3(fileName string) {
 	text := ReadText(fileName)
 	mul := regexp.MustCompile("mul\\((\\d{1,3}),(\\d{1,3})\\)")
-	matches := mul.FindAllStringSubmatch(text, -1)
 	res := 0
-	for _, match := range matches {
+	for {
+		match := mul.FindStringSubmatch(text)
+		if match == nil {
+			break
+		}
+		nextIndex := strings.Index(text, match[0])
+		disableIndex := strings.Index(text, "don't()")
+		if disableIndex != -1 && disableIndex < nextIndex {
+			text = text[disableIndex+1:]
+			enableIndex := strings.Index(text, "do()")
+			if enableIndex == -1 {
+				break
+			}
+			text = text[enableIndex+1:]
+			continue
+		}
 		res += StrToInt(match[1]) * StrToInt(match[2])
+		text = text[nextIndex+1:]
 	}
 	fmt.Println(res)
 }
