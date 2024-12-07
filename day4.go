7adbb94490972fe238da3818d3dcd244d3cc58eb
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-07 20:53:35 +0100

    day 4: solve part 2

--- day4.go
+++ day4.go
@@ -2,43 +2,21 @@ package main
 
 import "fmt"
 
-type direction struct {
-	dx int
-	dy int
-}
-
 func day4(fileName string) {
 	lines := ReadLines(fileName)
 	width := len(lines[0])
 	height := len(lines)
 
-	allDirections := []direction{
-		{-1, -1},
-		{-1, 0},
-		{-1, 1},
-		{0, -1},
-		{0, 1},
-		{1, -1},
-		{1, 0},
-		{1, 1},
-	}
-	wordSearch := "XMAS"
-	dLen := len(wordSearch) - 1
 	matches := 0
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			for _, d := range allDirections {
-				if x+d.dx*dLen < 0 || x+d.dx*dLen >= width ||
-					y+d.dy*dLen < 0 || y+d.dy*dLen >= height {
-					continue
-				}
-				word := make([]byte, len(wordSearch))
-				for i := 0; i <= dLen; i++ {
-					word[i] = lines[y+d.dy*i][x+d.dx*i]
-				}
-				if wordSearch == string(word) {
-					matches++
-				}
+	for x := 1; x < width-1; x++ {
+		for y := 1; y < height-1; y++ {
+			if lines[y][x] != 'A' {
+				continue
+			}
+			diag1 := string(lines[y-1][x-1]) + string(lines[y+1][x+1])
+			diag2 := string(lines[y+1][x-1]) + string(lines[y-1][x+1])
+			if (diag1 == "MS" || diag1 == "SM") && (diag2 == "MS" || diag2 == "SM") {
+				matches++
 			}
 		}
 	}
