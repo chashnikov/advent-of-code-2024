bb2e70ec6a5e252ce16e89ad40a28f75599b2048
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-14 22:22:58 +0100

    day 9: solve part 2

--- day9.go
+++ day9.go
@@ -4,42 +4,45 @@ import "fmt"
 
 func day9(fileName string) {
 	text := ReadText(fileName)
-	disk := make([]int, 10*len(text))
+	files := make([]*diskRange, 0)
+	free := make([]*diskRange, 0)
 	index := 0
 	fileId := 0
-	end := 0
 	fill := true
 	for _, char := range text {
 		digit := int(char - '0')
 		if fill {
-			for i := 0; i < digit; i++ {
-				disk[index+i] = fileId
-			}
-			end = index + digit - 1
+			files = append(files, &diskRange{index, digit})
 			fileId++
 		} else {
-			for i := 0; i < digit; i++ {
-				disk[index+i] = -1
-			}
+			free = append(free, &diskRange{index, digit})
 		}
 		index += digit
 		fill = !fill
 	}
-	res := 0
-	index = 0
-	for index <= end {
-		if disk[index] == -1 {
-			for end >= index && disk[end] == -1 {
-				end--
-			}
-			if index > end {
+	fileId--
+	for fileId > 0 {
+		length := files[fileId].len
+		for _, r := range free {
+			if r.len >= length && r.start < files[fileId].start {
+				files[fileId].start = r.start
+				r.start += length
+				r.len -= length
 				break
 			}
-			disk[index] = disk[end]
-			end--
 		}
-		res += disk[index] * index
-		index++
+		fileId--
+	}
+	res := int64(0)
+	for id, file := range files {
+		for i := 0; i < file.len; i++ {
+			res += int64(file.start+i) * int64(id)
+		}
 	}
 	fmt.Println(res)
+}
+
+type diskRange struct {
+	start int
+	len   int
 }
