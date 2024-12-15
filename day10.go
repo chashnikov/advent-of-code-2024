fef3bfc78434685e8afaae4bb5d8d12323f03f14
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-15 21:01:03 +0100

    day 10: solve part 2

--- day10.go
+++ day10.go
@@ -6,21 +6,15 @@ func day10(fileName string) {
 	lines := ReadLines(fileName)
 	width := len(lines[0])
 	height := len(lines)
-	nines := make([]intVector, 0, 10)
-	nineToIndex := make(map[intVector]uint)
-	trailheads := make([][]*bitset, height)
+	ratings := make([][]uint64, height)
 	for y := 0; y < height; y++ {
-		trailheads[y] = make([]*bitset, width)
+		ratings[y] = make([]uint64, width)
 	}
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			trailheads[y][x] = newBitset()
+			ratings[y][x] = 0
 			if lines[y][x] == '9' {
-				p := intVector{x, y}
-				index := uint(len(nines))
-				nineToIndex[p] = index
-				nines = append(nines, p)
-				trailheads[y][x].Set(index, true)
+				ratings[y][x] = 1
 			}
 		}
 	}
@@ -32,27 +26,27 @@ func day10(fileName string) {
 			for y := 0; y < height; y++ {
 				if lines[y][x] == char {
 					if x > 0 && lines[y][x-1] == prevChar {
-						trailheads[y][x].OrWith(trailheads[y][x-1])
+						ratings[y][x] += ratings[y][x-1]
 					}
 					if x < width-1 && lines[y][x+1] == prevChar {
-						trailheads[y][x].OrWith(trailheads[y][x+1])
+						ratings[y][x] += ratings[y][x+1]
 					}
 					if y > 0 && lines[y-1][x] == prevChar {
-						trailheads[y][x].OrWith(trailheads[y-1][x])
+						ratings[y][x] += ratings[y-1][x]
 					}
 					if y < height-1 && lines[y+1][x] == prevChar {
-						trailheads[y][x].OrWith(trailheads[y+1][x])
+						ratings[y][x] += ratings[y+1][x]
 					}
 				}
 			}
 		}
 	}
 
-	score := 0
+	var score uint64 = 0
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			if lines[y][x] == '0' {
-				score += trailheads[y][x].BitCount()
+				score += ratings[y][x]
 			}
 		}
 	}
