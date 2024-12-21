b6fe3f89d7439bf4437754d9fdba739cb16aa932
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-21 17:25:55 +0100

    day 13: solve part 2

--- day13.go
+++ day13.go
@@ -9,23 +9,23 @@ func day13(fileName string) {
 	lines := ReadLines(fileName)
 	buttonRegexp := regexp.MustCompile("Button .: X\\+(\\d+), Y\\+(\\d+)")
 	prizeRegexp := regexp.MustCompile("Prize: X=(\\d+), Y=(\\d+)")
-	res := 0
+	res := int64(0)
 	for m := 0; m < len(lines); m += 4 {
 		aMatch := buttonRegexp.FindStringSubmatch(lines[m])
 		bMatch := buttonRegexp.FindStringSubmatch(lines[m+1])
 		prizeMatch := prizeRegexp.FindStringSubmatch(lines[m+2])
-		aX := StrToInt(aMatch[1])
-		aY := StrToInt(aMatch[2])
-		bX := StrToInt(bMatch[1])
-		bY := StrToInt(bMatch[2])
-		pX := StrToInt(prizeMatch[1])
-		pY := StrToInt(prizeMatch[2])
+		aX := StrToInt64(aMatch[1])
+		aY := StrToInt64(aMatch[2])
+		bX := StrToInt64(bMatch[1])
+		bY := StrToInt64(bMatch[2])
+		pX := StrToInt64(prizeMatch[1]) + 10000000000000
+		pY := StrToInt64(prizeMatch[2]) + 10000000000000
 		res += ComputeMinimumTokens(aX, aY, bX, bY, pX, pY)
 	}
 	fmt.Println(res)
 }
 
-func ComputeMinimumTokens(aX int, aY int, bX int, bY int, pX int, pY int) int {
+func ComputeMinimumTokens(aX int64, aY int64, bX int64, bY int64, pX int64, pY int64) int64 {
 	// a*aX+b*bX = pX
 	// a*aY+b*bY = pY
 	det := aX*bY - aY*bX
