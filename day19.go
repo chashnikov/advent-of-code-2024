4ca300b10e15a1196c5c6313a6d0423216ebb038
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-12-22 14:50:11 +0100

    day 19: solve part 2

--- day19.go
+++ day19.go
@@ -8,23 +8,23 @@ import (
 func day19(fileName string) {
 	lines := ReadLines(fileName)
 	patterns := strings.Split(lines[0], ", ")
-	fmt.Println(Count(lines[2:], func(design string) bool {
-		return isTowelDesignPossible(design, patterns)
-	}))
+	fmt.Println(Sum(Map(lines[2:], func(design string) int {
+		return computeNumberOfTowelDesigns(design, patterns)
+	})))
 }
 
-func isTowelDesignPossible(design string, patterns []string) bool {
-	reachable := make([]bool, len(design)+1)
-	reachable[0] = true
+func computeNumberOfTowelDesigns(design string, patterns []string) int {
+	count := make([]int, len(design)+1)
+	count[0] = 1
 	for i := 0; i < len(design); i++ {
-		if !reachable[i] {
+		if count[i] == 0 {
 			continue
 		}
 		for _, pattern := range patterns {
 			if i+len(pattern) <= len(design) && design[i:i+len(pattern)] == pattern {
-				reachable[i+len(pattern)] = true
+				count[i+len(pattern)] += count[i]
 			}
 		}
 	}
-	return reachable[len(design)]
+	return count[len(design)]
 }
